4764328a02ef6cf6ff14f9f8ed8c443c5c6911d7
Emmanuel Borges Nakashima <emmanuel.nakashima@neki.corp>
2018-12-27 13:26:35 -0200

    actions and reducers

---
+++ src/actions/shared.js
@@ -0,0 +1,17 @@
+import { getInicialData } from '../utils/api'
+import { receiveUsers } from '../actions/users'
+import { receiveTweets } from '../actions/tweets'
+import { setAuthedUser } from '../actions/authedUser'
+
+const AUTHED_ID = 'tylermcginnis'
+
+export function handleInitialData () {
+    return (dispatch) => {
+        return getInicialData()
+            .then(({ users, tweets }) => {
+                dispatch(receiveUsers(users))
+                dispatch(receiveTweets(tweets))
+                dispatch(setAuthedUser(AUTHED_ID))
+            })
+    }
+}
